bb1e3ba5e45f5d1111a3d0cbfc8e7be4efd2731b
polekkieee <mrsampolak@gmail.com>
2026-04-02 11:38:50 +0200

    fixed the links to the bookretreat.com website

--- book-retreats-nl/app/page.tsx
+++ book-retreats-nl/app/page.tsx
@@ -258,7 +258,7 @@ export default function Home() {
             {filteredRetreats.map((retreat) => (
               <div
                 key={retreat.id}
-                onClick={() => window.open("https://bookretreats.com" + retreat.affiliateLink)}
+                onClick={() => window.open(retreat.affiliateLink)}
                 className="group cursor-pointer flex flex-col h-full relative" // relative toegevoegd
               >
 
